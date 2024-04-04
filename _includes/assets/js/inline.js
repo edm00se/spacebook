01fb9701e2ae809263e92d27b9ca0bf5fc8f018c
Eric McCormick <edm00se@users.noreply.github.com>
2024-04-04 16:30:24 +0000

    rips remaining cloudflare and netlify references

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,13 +1,3 @@
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
-      });
-    }
-  });
-}
-
 document.addEventListener("DOMContentLoaded", function() {
 
   const el = document.getElementById("main");
